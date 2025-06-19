6dcc7875f1b2d600044fe7c07738b52687a86862
a-2280 <khaliff.williamss@gmail.com>
2025-06-18 21:25:05 -0600

    added fallback project id and dataset for sanity

--- sanity/env.js
+++ sanity/env.js
@@ -1,5 +1,6 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-06-18'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-06-18";
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+export const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "syhv6kt4";
