4c3e0f5079c7abbc960a8ba4998ead7ab573b6b3
a-2280 <khaliff.williamss@gmail.com>
2025-06-18 21:46:19 -0600

    attempting to preload the background texture

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" href="/background_texture_image.png" as="image" />
+      </head>
       <body className="bg-[url(/background_texture_image.png)] bg-repeat bg-contain flex justify-center overflow-x-hidden">
         {children}
       </body>
