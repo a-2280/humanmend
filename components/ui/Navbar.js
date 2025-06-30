ac4446bd44b82921f52bcef517ccd1861058296f
a-2280 <khaliff.williamss@gmail.com>
2025-06-30 00:52:02 -0600

    small fixes getting ready for final version

--- components/ui/Navbar.js
+++ components/ui/Navbar.js
@@ -81,7 +81,7 @@ export default function Navbar(props) {
           alt="burger menu"
           width={30}
           height={30}
-          className="fixed right-12 top-[59px] lg:absolute lg:right-12 lg:top-auto lg:hidden w-full max-w-[25px] md:max-w-[30px] z-[9999]"
+          className="fixed top-[59px] lg:absolute right-12 lg:top-auto lg:hidden w-full max-w-[25px] md:max-w-[30px] z-[9999]"
           onClick={() => setMobileOpen(!mobileOpen)}
         />
       </nav>
